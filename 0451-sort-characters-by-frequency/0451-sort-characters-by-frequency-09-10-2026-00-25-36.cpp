e610420c00aeb17cc9f4e2605c8e960d7c8a7516
Anuket singh <moonknight1125@gmail.com>
2026-09-10 00:25:31 +0530

    Time: 17 ms (5.21%), Space: 10.2 MB (97.82%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency-09-10-2026-00-25-36.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+     int freq[256]={0};
+     for(char ch:s){
+        freq[ch]++; 
+     }   
+     sort(s.begin(),s.end(),[&](char a,char b){
+       if (freq[a] != freq[b])
+    return freq[a] > freq[b];
+
+        return a < b;
+     });
+     return s;
+    }
+};
